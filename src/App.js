4cd7fa25a47ee557ad952e1510cab0923992a2f0
tbuff10 <tbuff100@gmail.com>
2021-01-30 21:47:40 -0500

    Chnaged app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          I was changed on the future branch
         </a>
       </header>
     </div>
